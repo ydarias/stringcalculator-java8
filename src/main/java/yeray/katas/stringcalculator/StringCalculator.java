70c5a024a29795fc2f2326e8a83058c69b16d161
Yeray Darias <ydarias@gmail.com>
2016-06-13 23:30:03 +0200

    Can sum inputs of length 1

--- src/main/java/yeray/katas/stringcalculator/StringCalculator.java
+++ src/main/java/yeray/katas/stringcalculator/StringCalculator.java
@@ -3,6 +3,9 @@ package yeray.katas.stringcalculator;
 public class StringCalculator {
 
     public int add(String input) {
+        if (input.length() == 1)
+            return Integer.valueOf(input);
+
         return 0;
     }
 
--- src/test/java/yeray/katas/stringcalculator/StringCalculatorTest.java
+++ src/test/java/yeray/katas/stringcalculator/StringCalculatorTest.java
@@ -13,4 +13,11 @@ public class StringCalculatorTest {
         assertEquals(0, calculator.add(""));
     }
 
+    @Test
+    public void sumsInputsOfLengthOne() throws Exception {
+        StringCalculator calculator = new StringCalculator();
+
+        assertEquals(1, calculator.add("1"));
+    }
+
 }
